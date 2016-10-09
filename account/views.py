774d99d720d3a6642a2a701ed8819686f5905b88
lvchunhui7 <lvchunhui7@126.com>
2016-10-09 12:52:20 +0800

    coupon

--- account/views.py
+++ account/views.py
@@ -672,34 +672,23 @@ def coupon(request):
         'interest_num' : coupons.filter(project__ctype='1').count(),
         'exc_num' : coupons.filter(project__ctype='2').count()
     }
-    return render(request, 'account/account_coupon.html', {'dict':dict})
+    return render(request, 'account/m_account_coupon.html', {'dict':dict})
+
 def user_coupon_json(request):
     res={'code':0,}
-    if not request.user.is_authenticated():
-        res['code'] = -1
-        res['url'] = reverse('login') + "?next=" + reverse('account_coupon')
-        return JsonResponse(res)
-    page = request.GET.get("page", None)
-    size = request.GET.get("size", 2)
-    filter = request.GET.get("filter", '')
-    try:
-        size = int(size)
-    except ValueError:
-        size = 2
-    if not page or not filter or size <= 0:
-        raise Http404
-    item_list = Coupon.objects.filter(user=request.user,project__ctype=str(filter),is_used=False).select_related('project')
-    paginator = Paginator(item_list, size)
-    try:
-        contacts = paginator.page(page)
-    except PageNotAnInteger:
-    # If page is not an integer, deliver first page.
-        contacts = paginator.page(1)
-    except EmptyPage:
-    # If page is out of range (e.g. 9999), deliver last page of results.
-        contacts = paginator.page(paginator.num_pages)
+    count = int(request.GET.get('count', 0))
+    type = request.GET.get("type", '')
     data = []
-    for con in contacts:
+    count = int(count)
+    start = 6*count
+    try:
+        count = int(count)
+    except ValueError:
+        count = 0
+    if not count or not type:
+        raise Http404
+    item_list = Coupon.objects.filter(user=request.user,project__ctype=str(filter)).select_related('project').order_by('-time')[start:start+6]
+    for con in item_list:
         project = con.project
         i = {"title":project.title,
              "amount":project.amount,
@@ -710,12 +699,7 @@ def user_coupon_json(request):
              'code':con.exchange_code
         }
         data.append(i)
-    if data:
-        res['code'] = 1
-    res["pageCount"] = paginator.num_pages
-    res["recordCount"] = item_list.count()
-    res["data"] = data
-    return JsonResponse(res)
+    return JsonResponse(data,safe=False)
 def get_user_coupon_exchange_detail(request):
     res={'code':0,}
     if not request.user.is_authenticated():
