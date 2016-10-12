32c4798acf5418d9d40f5e2e06e33a1af98b965f
lch <18500581509@163.com>
2016-10-12 09:28:37 +0800

    strategy and press

--- wafuli/views.py
+++ wafuli/views.py
@@ -111,37 +111,21 @@ def press(request, id):
         press = Press.objects.get(id=id)
     except Press.DoesNotExist:
         raise Http404(u"该页面不存在")
-    return render(request, 'detail-press.html',{'press':press})
+    return render(request, 'm_detail_press.html',{'press':press})
 
 def aboutus(request):
     ad_list = Advertisement.objects.filter(location__in=['0','6'],is_hidden=False).first
     return render(request, 'aboutus.html',{'ad_list':ad_list})
 
 def strategy(request):
-#     item_list = Press.objects
-#     type = str(type)
-#     strategy_list = item_list.filter(type=type)
-#     strategy_list = item_list.filter(type=type)
-#     strategy_list = item_list.filter(type=type)
-#     paginator = Paginator(item_list, size)
-#     try:
-#         contacts = paginator.page(page)
-#     except PageNotAnInteger:
-#     # If page is not an integer, deliver first page.
-#         contacts = paginator.page(1)
-#     except EmptyPage:
-#     # If page is out of range (e.g. 9999), deliver last page of results.
-#         contacts = paginator.page(paginator.num_pages)
-#     data = []
-#     for con in contacts:        
-#         i = {"title":con.title,
-#              "url":con.url,
-#              "time":con.pub_date.strftime("%Y-%m-%d"),
-#              "view":con.view_count,
-#              "summary":con.summary,
-#         }
-#         data.append(i)
-    return render(request, 'm_strategy.html')
+    item_list = Press.objects
+    strategy_list = item_list.filter(type='2')
+    notice_list = item_list.filter(type='1')
+    context = {
+            'strategy_list':strategy_list,
+            'notice_list':notice_list,
+        }
+    return render(request, 'm_strategy.html', context)
 # def experience_taskandfinance(request):
 #     if not request.is_ajax():
 #         logger.warning("Experience refused no-ajax request!!!")
