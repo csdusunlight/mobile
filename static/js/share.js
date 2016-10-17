46721cbcc1db7dc01c30114f348a8de21957dbda
lvchunhui7 <lvchunhui7@126.com>
2016-10-17 22:02:32 +0800

    share

--- static/js/share.js
+++ static/js/share.js
@@ -13,11 +13,19 @@ var div = document.createElement('div');
 					'<a class="jiathis_button_qzone">QQ空间</a>'+
 					'<a class="jiathis_button_tsina">新浪微博</a>'+
 					'<a class="jiathis_button_tqq">腾讯微博</a>'+
-					'<a id="share_pengyouquan" class="jiathis_button_weixin">微信</a>'+
+					'<a class="jiathis_button_weixin">微信</a>'+
 					'<a class="jiathis_button_cqq">QQ好友</a>'+
 					'</div></div></div></div>';
 	body.appendChild(div);
 
+var oScript= document.createElement("script");
+	oScript.type = "text/javascript";
+	oScript.src="http://v3.jiathis.com/code/jia.js";
+	body.appendChild( oScript);
+
+//		监听点击事件
+
+//		点击分享按钮弹出弹窗
 share.addEventListener("tap",function () {
 
 	var share_bg = document.getElementById("share_bg"),
@@ -33,41 +41,9 @@ share_bg.addEventListener("tap",function () {
 	share_bg.style.display = "none";
   	content_box.setAttribute("class","content-box-02");
 });
-wx.config({
-    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-    appId: appId, // 必填，公众号的唯一标识
-    timestamp: timestamp, // 必填，生成签名的时间戳
-    nonceStr: nonceStr, // 必填，生成签名的随机串
-    signature: signature,// 必填，签名，见附录1
-    jsApiList: [
-    	'onMenuShareTimeline',
-    	'onMenuShareAppMessage',
-    	'onMenuShareQQ',
-    	'onMenuShareQZone',
-    ] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-});
-var share_pyq = document.getElementById("share_pengyouquan");
-alert('ddd');
-wx.ready(function () {
-    wx.checkJsApi({
-        jsApiList: [
-            'onMenuShareTimeline',
-        ],
-    });
-	wx.onMenuShareTimeline({
-	    title: 'nihaoma', // 分享标题
-	    link: share_url, // 分享链接
-	    imgUrl: 'http://m.wafuli.cn/static/images/back01.jpg', // 分享图标
-	    success: function () { 
-	        alert('yes');// 用户确认分享后执行的回调函数
-	    },
-	    cancel: function () {
-	    	alert('no');
-	        // 用户取消分享后执行的回调函数
-	    }
-	});
-    wx.error(function(res){
-        // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
-
-    });
-});
+//		参数设置
+var jiathis_config = {
+	summary: "",
+	shortUrl: false,
+	hideMore: false
+}
--- wafuli/templatetags/myfilter.py
+++ wafuli/templatetags/myfilter.py
@@ -3,3 +3,7 @@ register = template.Library()
 @register.filter('list')
 def do_list(value):
     return range(1, value+1)
+
+@register.filter('modulo')
+def do_modulo(value, arg):
+    return value%int(arg)
