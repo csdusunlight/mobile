07e760e8754b91f37fce723d29faae99d89757ce
jiangzongyang <jiangzongyang197@163.com>
2016-10-24 16:38:23 +0800

    share

--- static/js/share.js
+++ static/js/share.js
@@ -3,7 +3,7 @@ var body = document.getElementsByTagName("body")[0],
 //		动态创建HTML
 var div = document.createElement('div');
 var ua = navigator.userAgent;
-if((ua.match(/MicroMessenger/i)=="micromessenger") || (isIos && ua.indexOf('MQQBrowser QQ') > -1)) {
+if(ua.match(/MicroMessenger/i)=="micromessenger") {
     div.className = 'share-guide-box share-top';
 } else {
     div.className = 'share-guide-box share-bottom';
