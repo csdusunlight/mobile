ad4adaf10bbdf51286c4e1a9e70d3fc092e67102
lvchunhui7 <lvchunhui7@126.com>
2016-11-09 19:52:00 +0800

    app

--- app/views.py
+++ app/views.py
@@ -9,8 +9,11 @@ def get_news(request):
     timestamp = request.GET.get('lastDate','')
     if not timestamp:
         return JsonResponse('',safe=False)
-    lastDate = datetime.fromtimestamp(float(timestamp)/1000)
-    now = datetime.now()
+    lastDate = None
+    try:
+        lastDate = datetime.fromtimestamp(float(timestamp)/1000)
+    except:
+        lastDate = datetime.now()
     last_wel_list = Welfare.objects.filter(is_display=True,state='1',startTime__lte=lastDate).\
         exclude(type='baoyou').order_by("-startTime")[0:10]
     ret_list = []
@@ -37,48 +40,27 @@ def get_slider(request):
     for adv in adv_list:
         attr_dic = {
             'id':adv.id,
-            'image': host + adv.pic.url,
+            'image': host + adv.mpic.url,
             'priority': adv.news_priority,
             'pubDate': adv.pub_date,
         }
         ret_list.append(attr_dic)
     return JsonResponse(ret_list,safe=False)
 def get_recom(request):
-    adv_list = list(Advertisement.objects.filter(location__in=['0','1'],is_hidden=False)[0:5])
-    first_adv = adv_list[0] if adv_list else None
-    last_adv = adv_list[-1] if adv_list else None
-    now = datetime.now()
-    start = now - timedelta(days=1)
     adv_today1 = MAdvert.objects.filter(location='1',is_hidden=False).first()
     adv_today2 = MAdvert.objects.filter(location='2',is_hidden=False).first()
     adv_today3 = MAdvert.objects.filter(location='3',is_hidden=False).first()
-    adv_today1_info = {'pic_url':adv_today1.pic.url, 'url':adv_today1.url}
-    adv_today2_info = {'pic_url':adv_today2.pic.url, 'url':adv_today2.url}
-    adv_today3_info = {'pic_url':adv_today3.pic.url, 'url':adv_today3.url}
-    adv_info_list = []
-    for adv in adv_list:
-        dic = {}
-        dic['pic_url'] = host + str(adv.mpic.url)
-        dic['url'] = adv.url
-        adv_info_list.append(dic)
-    context = {'adv_info_list':adv_info_list,
-               'adv_today1_info':adv_today1_info,
-               'adv_today2_info':adv_today2_info,
-               'adv_today3_info':adv_today3_info,
-    }
-    try:
-        statis = DayStatis.objects.get(date=date.today())
-    except:
-        new_wel_num = 0
-    else:
-        new_wel_num = statis.new_wel_num
-    glo_statis = GlobalStatis.objects.first()
-    if glo_statis:
-        all_wel_num = glo_statis.all_wel_num
-        withdraw_total = glo_statis.award_total
-        
-    else:
-        withdraw_total = 0
-        all_wel_num = 0
-    context.update({'new_wel_num':new_wel_num, 'all_wel_num':all_wel_num, 'withdraw_total':withdraw_total})
-    return JsonResponse(context)
+    ret_list = [{
+        'id':adv_today1.id,
+        'image': host + adv_today1.pic.url,
+        'location': 1,
+    },{
+        'id':adv_today2.id,
+        'image': host + adv_today2.pic.url,
+        'location': 2,
+    },{
+        'id':adv_today3.id,
+        'image': host + adv_today3.pic.url,
+        'location': 3,
+    }]
+    return JsonResponse(ret_list,safe=False)
