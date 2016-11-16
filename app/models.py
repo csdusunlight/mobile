3f6f619c9e8a441d0eac958d8825cba7b89d76e6
lch <18500581509@163.com>
2016-11-16 17:14:39 +0800

    login

--- app/models.py
+++ app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from account.models import MyUser
 
 # Create your models here.
+class UserToken(models.Model):
+    token = models.CharField("token", max_length=32, primary_key=True)
+    user = models.ForeignKey(MyUser,related_name = 'tokens',)
+    expire = models.IntegerField(u"expire_time")
