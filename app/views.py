b61e6b8736a1d102fbde3ee7a07f364390990100
lch <18500581509@163.com>
2017-08-18 16:48:02 +0800

    updown

--- app/views.py
+++ app/views.py
@@ -144,6 +144,8 @@ def get_content_hongbao(request):
         'title':wel.title,
         'subtitle':wel.subtitle,
         'htype':wel.htype,
+        'up':wel.up,
+        'down':wel.down,
     }
     return JsonResponse(ret_dict)
 def get_content_youhuiquan(request):
